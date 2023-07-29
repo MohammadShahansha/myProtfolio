ebdb47d24d2aff28cf1105a0a5007eca6d469554
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2023-07-29 15:33:36 +0600

    added link

--- src/component/Banner/Benner.jsx
+++ src/component/Banner/Benner.jsx
@@ -34,7 +34,7 @@ const Benner = () => {
                 {/* <p className='mb-6 md:mb-auto'>I am Mohammad Shahansha.I am a Bangladeshi.I read in Comilla University in 3rd year and My subject is B.Sc Engineering in Information & Communication Technology. </p> */}
                 <div className='bttn-box'>
                     <a href="mailto:mohammadshahansha111@gmail.com" className='bttn'>Hire Me</a>
-                    <a href="https://drive.google.com/file/d/1lGViYlxTSLeyvTNKVEG3xTg_Elfu6lR3/view?usp=sharing" className='bttn'>Resume</a>
+                    <a href="https://drive.google.com/file/d/18T1XlwhtgrwzeV7TzR_0pIJYgDY8PPU0/view?usp=sharing" className='bttn'>Resume</a>
                 </div>
                 <div className='socials mx-auto md:ms-0'>
                     <a href="https://www.facebook.com/profile.php?id=100083540903880&mibextid=ZbWKwL"><BsFacebook></BsFacebook></a>
